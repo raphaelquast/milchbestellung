e3c323ba6579b3b0a6325420ffc898a4c3e46722
Raphael Quast <rquast@geo.tuwien.ac.at>
2018-11-25 14:44:43 +0100

    update of setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup file for milchbestellung
 """
 
 from setuptools import setup
-from setuptools import find_packages
+#from setuptools import find_packages
 
 install_requires = ["numpy", "pandas"]
 
 setup(name='milchbestellung',
       version='v0.0.1',
       description='milchbestellung',
-      package_dir={'milchbestellung': 'milchbestellung'},
+      packages=['milchbestellung'],
       install_requires=install_requires,
       )
 
