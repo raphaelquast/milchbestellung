233a935581d451356b67fa13d840050ee67b0f8e
Raphael Quast <rquast@geo.tuwien.ac.at>
2018-11-26 22:43:23 +0100

    update

--- milchbestellung/milchbestellung.py
+++ milchbestellung/milchbestellung.py
@@ -636,23 +636,20 @@ class gui(milchliste):
             self.returned_values['foldername'] = filedialog.askdirectory()
             _ = read_callback()
 
-        def generate_files():
-            if 'foldername' in self.returned_values:
-                _ = getresult(['KW'] + self.vorratsliste + self.additional_vorrat_indexes,
-                              [KW_var] + vorrat_vars)
-
-                self.milchlisten_erzeugen()
-            else:
-                print('select foldername first')
 
         def getresult(names, variables):
             for name, var in zip(names, variables):
                 self.returned_values[name] = var.get()
             return
 
-
+        frame_0 = tk.Frame(self.root)
         # add a button for file-dialog
-        tk.Button(self.root, text='Ordner auswählen', command=open_file_dialog).pack()
+        tk.Button(frame_0, text='Ordner auswählen', command=open_file_dialog).grid(row=0, column=0, padx=20)
+
+        warn_variable = tk.StringVar()
+        warn_variable.set('')
+        warnlabel = tk.Label(frame_0, textvariable=warn_variable)
+        warnlabel.grid(row=0, column=2, padx=20)
 
         # # Additional vorrat products - variable
         # # initialize frame for additional products
@@ -671,14 +668,14 @@ class gui(milchliste):
 
         # Kalenderwochen-variable
         KW_var = tk.StringVar(value='0')
-        tk.Label(frame, text = 'Aktuelle Kalenderwoche:   KW').grid(row=0, column=0)
+        tk.Label(frame, text = 'Aktuelle Kalenderwoche:   KW').grid(row=0, column=0 ,sticky='E')
         tk.Entry(frame, textvariable=KW_var).grid(row=0, column=1)
 
 
 
         # header
         #tk.Label(frame, text = 'Produktname').grid(row=1, column=0)
-        tk.Label(frame, text = '').grid(row=1, column=1)
+        #tk.Label(frame, text = 'asdf').grid(row=1, column=1)
         tk.Label(frame, text = 'Mindestbestellmenge').grid(row=1, column=2)
         tk.Label(frame, text = 'Bestellt').grid(row=1, column=3)
         tk.Label(frame, text = 'Total').grid(row=1, column=4)
@@ -692,6 +689,7 @@ class gui(milchliste):
 
         vorrat_labels = [tk.StringVar(value=name) for name in self.vorratsliste + self.additional_vorrat_indexes]
 
+        tot_best_menge_labels = []
         vorrat_entrys = []
         for i, [name, vorrat_var, best_var, mind_best_var, tot_best_var, vorrat_label] in enumerate(zip(self.vorratsliste + self.additional_vorrat_indexes,
                                                     vorrat_vars,
@@ -706,19 +704,20 @@ class gui(milchliste):
             # add an entry-cell to each row
             entry = tk.Entry(frame, textvariable = vorrat_var)
             vorrat_entrys += [entry]
-            entry.grid(row=i + 2, column=1)
+            entry.grid(row=i + 2, column=1, padx=20)
 
             # add mindestbestellmenge to each row
             tk.Label(frame, textvariable = mind_best_var
-                     ).grid(row=i + 2, column=2, sticky=tk.E)
+                     ).grid(row=i + 2, column=2, sticky=tk.E, padx=20)
             # add mindestbestellmenge to each row
             tk.Label(frame, textvariable = best_var
-                     ).grid(row=i + 2, column=3, sticky=tk.E)
+                     ).grid(row=i + 2, column=3, sticky=tk.E, padx=20)
             # add total bestellmenge to each row
-            tk.Label(frame, textvariable = tot_best_var
-                     ).grid(row=i + 2, column=4, sticky=tk.E)
+            _totlabel = tk.Label(frame, textvariable = tot_best_var)
+            tot_best_menge_labels += [_totlabel]
+            _totlabel.grid(row=i + 2, column=4, sticky=tk.E, padx=20)
 
-        frame.pack()
+        frame.pack(fill='x')
 
 
         def read_callback():
@@ -736,9 +735,12 @@ class gui(milchliste):
             #vorratprod_bestellungen = [self.milchlisten_dict['sumorders'].loc[i].values.flatten()[0] for i in self.milchlisten_dict['vorrats_index']]
 
             for i, [mind, best] in enumerate(zip(mind_bestellmengen, vorratprod_bestellungen)):
+                # mindest bestellmenge setzen
                 mind_best_vars[i].set(mind)
+
                 # bestellte menge setzen
                 best_vars[i].set(best)
+
                 # bestellmenge - mindestbestellmenge in das entry-feld schreiben
                 if float(best) == 0.:
                     vorrat_vars[i].set(0.)
@@ -749,7 +751,7 @@ class gui(milchliste):
 
                 # totale bestellung setzen
                 tot_best_vars[i].set(str(np.round(float(vorrat_vars[i].get()) + float(best), 2)))
-
+                tot_best_menge_labels[i].config(bg='green')
             # TODO
             for vorrat_label, add_vorrat_prod in zip(vorrat_labels, self.vorratsliste + self.additional_vorrat_indexes):
                 if add_vorrat_prod in self.additional_vorrat_indexes:
@@ -757,23 +759,54 @@ class gui(milchliste):
 
 
         def add_vorrat(event):
-            # totale bestellung setzen
-            for i, val in enumerate([self.milchlisten_dict['sumorders'].loc[i].values.flatten()[0] for i in self.milchlisten_dict['vorrats_index']]):
-                tot_best_vars[i].set(str(np.round(float(val) + float(vorrat_vars[i].get()), 2)))
+            if 'foldername' not in self.returned_values:
+                warn_variable.set('Zuerst Ordner auswählen!')
+                warnlabel.config(bg='red')
+            else:
+                mind_bestellmengen=[self.milchlisten_dict['vorrats_bestellmengen'][i] for i in self.milchlisten_dict['vorrats_index']]
+                # totale bestellung setzen
+                for i, val in enumerate([self.milchlisten_dict['sumorders'].loc[i].values.flatten()[0] for i in self.milchlisten_dict['vorrats_index']]):
+                    tot_best_vars[i].set(str(np.round(float(val) + float(vorrat_vars[i].get()), 2)))
+                    if np.round(float(val) + float(vorrat_vars[i].get()), 2) % mind_bestellmengen[i] == 0:
+                        tot_best_menge_labels[i].config(bg="green")
+                    else:
+                        tot_best_menge_labels[i].config(bg="red")
+
 
         for i in vorrat_entrys:
             i.bind("<Return>", add_vorrat)
 
 
+        def generate_files():
+            # check if folder has been selected
+            if 'foldername' not in self.returned_values:
+                warn_variable.set('Zuerst Ordner auswählen!')
+                warnlabel.config(bg='red')
 
-        # tk.Button(self.root, text="read list",
-        #           command=read_callback).pack()
+            else:
+                # check if all orders are correct
+                bestellmengen_OK = True
+                mind_bestellmengen=[self.milchlisten_dict['vorrats_bestellmengen'][i] for i in self.milchlisten_dict['vorrats_index']]
+                for i, val in enumerate([self.milchlisten_dict['sumorders'].loc[i].values.flatten()[0] for i in self.milchlisten_dict['vorrats_index']]):
+                    if np.round(float(val) + float(vorrat_vars[i].get()), 2) % mind_bestellmengen[i] != 0:
+                        bestellmengen_OK = False
 
+                if bestellmengen_OK is False:
+                    warn_variable.set('Bestellmengen passen nicht zu Mindestbestellmengen!')
+                    warnlabel.config(bg='red')
+                else:
+                    warn_variable.set('Milchlisten werden erzeugt...')
+                    warnlabel.config(bg='green')
+                    self.root.update_idletasks()
+                    _ = getresult(['KW'] + self.vorratsliste + self.additional_vorrat_indexes,
+                                  [KW_var] + vorrat_vars)
 
+                    self.milchlisten_erzeugen()
 
         # add a button to generate the lists
-        tk.Button(self.root, text='Milchlisten erzeugen',
-                  command=generate_files).pack()
+        tk.Button(frame_0, text='Milchlisten erzeugen',
+                  command=generate_files).grid(row=0, column=1)
+        frame_0.pack(fill='both', padx=10, pady=10)
 
 
 
