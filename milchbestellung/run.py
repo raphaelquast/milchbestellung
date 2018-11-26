92dd97dc93a711e2fb2a85eec99723831f23b7cc
Raphael Quast <rquast@geo.tuwien.ac.at>
2018-11-26 23:23:39 +0100

    update

--- milchbestellung/run.py
+++ milchbestellung/run.py
@@ -1,9 +1,12 @@
-from milchbestellung.gui import gui
+from milchbestellung import gui
 import sys
 
 def run(arg):
+    # initialize class object
     asdf = gui()
+    # call gui
     asdf.gui(arg)
+
 
 if __name__ == '__main__':
     run(sys.argv[1:])
