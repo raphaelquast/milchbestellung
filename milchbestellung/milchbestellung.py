9199a2081b972401761b6cacdaeb0ff9cb983762
Raphael Quast <rquast@geo.tuwien.ac.at>
2018-11-28 14:58:20 +0100

    support for additional vorrat products

--- milchbestellung/milchbestellung.py
+++ milchbestellung/milchbestellung.py
@@ -9,6 +9,9 @@ import os
 
 import copy
 
+# link to get file:
+# https://docs.google.com/spreadsheets/d/1GXF9jgnnZ2YkY3InACf2qt1Q-L4nlMUy3ZrXndLhd2Q/gviz/tq?tqx=out:csv&sheet=Bestellfile
+
 class milchliste(object):
     def __init__(self):
         # spalten-nummer ab welcher die namen eingegeben werden (zähler startet bei 0!)
@@ -601,28 +604,29 @@ class gui(milchliste):
         # initialize main window
         self.root = tk.Tk()
 
-    #     self.frame = tk.Frame(self.root)
-    #     self.fields = [1,2,3]
-    #     self.createWidgets(self.frame)
-    #     self.frame.pack()
+        # initialize main frame
+        self.main_frame = tk.Frame(self.root)
+
+        # initialize bottom frame
+        self.frame_0 = tk.Frame(self.root)
+
+        # initialize a list of the total bestellmenge variables and labels
+        self.tot_best_vars = []
+        self.tot_best_menge_labels = []
+
+        # initialize a list for the vorrat entry-fields
+        self.vorrat_entrys = []
 
 
-    # def createWidgets(self, frame):
-    #     self.add_field(frame)
+        # initialize a warn variable
+        self.warn_variable = tk.StringVar()
+        self.warn_label = tk.Label(self.frame_0, textvariable=self.warn_variable)
 
-    #     self.add_lang = tk.Button(frame, text="add language", command=lambda : self.add_field(frame))
-    #     self.add_lang.grid(row=len(self.fields), column=2)
-
-
-    # def add_field(self, frame):
-    #     self.fields.append({})
-
-    #     n = len(self.fields) - 1
-    #     self.fields[n]['var'] = tk.StringVar()
-    #     self.fields[n]['field'] = tk.Entry(frame, textvariable=self.fields[n]['var'])
-    #     self.fields[n]['field'].grid(row=n, column=1)
-
-    #     tk.Label(frame, text=str(n) + "add language").grid(row=n, column=0, sticky=tk.W)
+        # initiate a frame for additional vorrat fields
+        self.add_vorrat_frame = tk.Frame(self.root)
+        # textvariable for entry field of additional vorrat prodcts
+        self.add_vorrat_set_var = tk.StringVar()
+        self.add_vorrat_tot_vars = []
 
 
     def gui(self, additional_vorrat_products=[]):
@@ -642,14 +646,11 @@ class gui(milchliste):
                 self.returned_values[name] = var.get()
             return
 
-        frame_0 = tk.Frame(self.root)
         # add a button for file-dialog
-        tk.Button(frame_0, text='Ordner auswählen', command=open_file_dialog).grid(row=0, column=0, padx=20)
+        tk.Button(self.frame_0, text='Ordner auswählen', command=open_file_dialog).grid(row=0, column=0, padx=20)
 
-        warn_variable = tk.StringVar()
-        warn_variable.set('')
-        warnlabel = tk.Label(frame_0, textvariable=warn_variable)
-        warnlabel.grid(row=0, column=2, padx=20)
+        self.warn_variable.set('')
+        self.warn_label.grid(row=0, column=2, padx=20)
 
         # # Additional vorrat products - variable
         # # initialize frame for additional products
@@ -663,61 +664,57 @@ class gui(milchliste):
 
 
 
-        # initialize frame
-        frame = tk.Frame(self.root)
 
         # Kalenderwochen-variable
         KW_var = tk.StringVar(value='0')
-        tk.Label(frame, text = 'Aktuelle Kalenderwoche:   KW').grid(row=0, column=0 ,sticky='E')
-        tk.Entry(frame, textvariable=KW_var).grid(row=0, column=1)
+        tk.Label(self.main_frame, text = 'Aktuelle Kalenderwoche:   KW').grid(row=0, column=0 ,sticky='E')
+        tk.Entry(self.main_frame, textvariable=KW_var).grid(row=0, column=1)
 
 
 
         # header
-        #tk.Label(frame, text = 'Produktname').grid(row=1, column=0)
-        #tk.Label(frame, text = 'asdf').grid(row=1, column=1)
-        tk.Label(frame, text = 'Mindestbestellmenge').grid(row=1, column=2)
-        tk.Label(frame, text = 'Bestellt').grid(row=1, column=3)
-        tk.Label(frame, text = 'Total').grid(row=1, column=4)
+        #tk.Label(self.main_frame, text = 'Produktname').grid(row=1, column=0)
+        #tk.Label(self.main_frame, text = 'asdf').grid(row=1, column=1)
+        tk.Label(self.main_frame, text = 'Mindestbestellmenge').grid(row=1, column=2)
+        tk.Label(self.main_frame, text = 'Bestellt').grid(row=1, column=3)
+        tk.Label(self.main_frame, text = 'Total').grid(row=1, column=4)
 
         nvorrat = len(self.vorratsliste + self.additional_vorrat_indexes)
 
-        vorrat_vars = [tk.StringVar(value='0') for i in range(nvorrat)]
-        best_vars = [tk.StringVar(value='--') for i in range(nvorrat)]
-        mind_best_vars = [tk.StringVar(value='--') for i in range(nvorrat)]
-        tot_best_vars = [tk.StringVar(value='--') for i in range(nvorrat)]
+        self.vorrat_vars = [tk.StringVar(value='0') for i in range(nvorrat)]
+        self.best_vars = [tk.StringVar(value='--') for i in range(nvorrat)]
+        self.mind_best_vars = [tk.StringVar(value='--') for i in range(nvorrat)]
+        self.tot_best_vars += [tk.StringVar(value='--') for i in range(nvorrat)]
 
         vorrat_labels = [tk.StringVar(value=name) for name in self.vorratsliste + self.additional_vorrat_indexes]
 
-        tot_best_menge_labels = []
-        vorrat_entrys = []
         for i, [name, vorrat_var, best_var, mind_best_var, tot_best_var, vorrat_label] in enumerate(zip(self.vorratsliste + self.additional_vorrat_indexes,
-                                                    vorrat_vars,
-                                                    best_vars,
-                                                    mind_best_vars,
-                                                    tot_best_vars,
+                                                    self.vorrat_vars,
+                                                    self.best_vars,
+                                                    self.mind_best_vars,
+                                                    self.tot_best_vars,
                                                     vorrat_labels)):
 
             # add a name to each row
-            tk.Label(frame, textvariable=vorrat_label).grid(row=i + 2, column=0, sticky=tk.W)
+            tk.Label(self.main_frame, textvariable=vorrat_label).grid(row=i + 2, column=0, sticky=tk.W)
 
             # add an entry-cell to each row
-            entry = tk.Entry(frame, textvariable = vorrat_var)
-            vorrat_entrys += [entry]
+            entry = tk.Entry(self.main_frame, textvariable = vorrat_var)
+            self.vorrat_entrys += [entry]
             entry.grid(row=i + 2, column=1, padx=20)
 
             # add mindestbestellmenge to each row
-            tk.Label(frame, textvariable = mind_best_var
+            tk.Label(self.main_frame, textvariable = mind_best_var
                      ).grid(row=i + 2, column=2, sticky=tk.E, padx=20)
-            # add mindestbestellmenge to each row
-            tk.Label(frame, textvariable = best_var
+            # add bestellmenge to each row
+            tk.Label(self.main_frame, textvariable = best_var
                      ).grid(row=i + 2, column=3, sticky=tk.E, padx=20)
             # add total bestellmenge to each row
-            _totlabel = tk.Label(frame, textvariable = tot_best_var)
-            tot_best_menge_labels += [_totlabel]
+            _totlabel = tk.Label(self.main_frame, textvariable = tot_best_var)
+            self.tot_best_menge_labels += [_totlabel]
             _totlabel.grid(row=i + 2, column=4, sticky=tk.E, padx=20)
 
-        frame.pack(fill='x')
+        self.main_frame.pack(fill='x')
 
 
         def read_callback():
@@ -736,22 +733,22 @@ class gui(milchliste):
 
             for i, [mind, best] in enumerate(zip(mind_bestellmengen, vorratprod_bestellungen)):
                 # mindest bestellmenge setzen
-                mind_best_vars[i].set(mind)
+                self.mind_best_vars[i].set(mind)
 
                 # bestellte menge setzen
-                best_vars[i].set(best)
+                self.best_vars[i].set(best)
 
                 # bestellmenge - mindestbestellmenge in das entry-feld schreiben
                 if float(best) == 0.:
-                    vorrat_vars[i].set(0.)
+                    self.vorrat_vars[i].set(0.)
                 elif float(best) > float(mind):
-                    vorrat_vars[i].set(str(float(mind)*np.ceil(float(best)/float(mind)) - float(best)))
+                    self.vorrat_vars[i].set(str(float(mind)*np.ceil(float(best)/float(mind)) - float(best)))
                 elif float(best) <= float(mind):
-                    vorrat_vars[i].set(str(np.round(float(mind) - float(best), 2)))
+                    self.vorrat_vars[i].set(str(np.round(float(mind) - float(best), 2)))
 
                 # totale bestellung setzen
-                tot_best_vars[i].set(str(np.round(float(vorrat_vars[i].get()) + float(best), 2)))
-                tot_best_menge_labels[i].config(bg='green')
+                self.tot_best_vars[i].set(str(np.round(float(self.vorrat_vars[i].get()) + float(best), 2)))
+                self.tot_best_menge_labels[i].config(bg='green')
             # TODO
             for vorrat_label, add_vorrat_prod in zip(vorrat_labels, self.vorratsliste + self.additional_vorrat_indexes):
                 if add_vorrat_prod in self.additional_vorrat_indexes:
@@ -760,56 +757,178 @@ class gui(milchliste):
 
         def add_vorrat(event):
             if 'foldername' not in self.returned_values:
-                warn_variable.set('Zuerst Ordner auswählen!')
-                warnlabel.config(bg='red')
+                self.warn_variable.set('Zuerst Ordner auswählen!')
+                self.warn_label.config(bg='red')
             else:
                 mind_bestellmengen=[self.milchlisten_dict['vorrats_bestellmengen'][i] for i in self.milchlisten_dict['vorrats_index']]
                 # totale bestellung setzen
-                for i, val in enumerate([self.milchlisten_dict['sumorders'].loc[i].values.flatten()[0] for i in self.milchlisten_dict['vorrats_index']]):
-                    tot_best_vars[i].set(str(np.round(float(val) + float(vorrat_vars[i].get()), 2)))
-                    if np.round(float(val) + float(vorrat_vars[i].get()), 2) % mind_bestellmengen[i] == 0:
-                        tot_best_menge_labels[i].config(bg="green")
+                for i, best_ind in enumerate(self.milchlisten_dict['vorrats_index']):
+                    try:
+                        val = self.milchlisten_dict['sumorders'].loc[best_ind].values.flatten()[0]
+                    except:
+                        # if index is not in orders
+                        val = '0.'
+
+                    self.tot_best_vars[i].set(str(np.round(float(val) + float(self.vorrat_vars[i].get()), 2)))
+                    if np.round(float(val) + float(self.vorrat_vars[i].get()), 2) % mind_bestellmengen[i] == 0:
+                        self.tot_best_menge_labels[i].config(bg="green")
                     else:
-                        tot_best_menge_labels[i].config(bg="red")
+                        self.tot_best_menge_labels[i].config(bg="red")
 
 
-        for i in vorrat_entrys:
+        for i in self.vorrat_entrys:
+            i.bind("<FocusOut>", add_vorrat)
             i.bind("<Return>", add_vorrat)
+
+
+        def add_field():
+            '''
+            add an additional vorrat product
+            '''
+            if 'foldername' not in self.returned_values:
+                self.warn_variable.set('Zuerst Ordner auswählen!')
+                self.warn_label.config(bg='red')
+                return
+
+            add_vorrat_index = self.add_vorrat_set_var.get()
+            if add_vorrat_index in self.milchlisten_dict['vorrats_index']:
+                self.warn_variable.set('Produkt bereits in liste!')
+                self.warn_label.config(bg='red')
+                return
+
+            #add_vorrat_Qs = self.milchlisten_dict['fulllist']['Art. Nr.:']==add_vorrat_index
+
+            zfilled_Art_Nrs = np.array([str(i).zfill(6) for i in asdf.milchlisten_dict['fulllist']['Art. Nr.:'].values.flatten()])
+            add_vorrat_Qs = zfilled_Art_Nrs == add_vorrat_index
+
+            if np.count_nonzero(add_vorrat_Qs) == 0:
+                self.warn_variable.set('Artikelnummer nicht gefunden')
+                self.warn_label.config(bg='red')
+                return
+            elif np.count_nonzero(add_vorrat_Qs) > 1:
+                self.warn_variable.set('Idente Artikelnummern gefunden!')
+                self.warn_label.config(bg='red')
+                return
+            else:
+                self.warn_variable.set('...adding ' + add_vorrat_index)
+                self.warn_label.config(bg='green')
+
+            self.additional_vorrat_indexes += [add_vorrat_index]
+            self.milchlisten_dict['vorrats_index'] += [add_vorrat_index]
+
+            #row number of the added field
+            n = len(self.tot_best_vars) + 3
+
+            # mindestbestellmengen setzen
+            # (if value can not be converted to float, use 1)
+            try:
+                mind_best=self.milchlisten_dict['fulllist']['Menge'][add_vorrat_Qs].values[0]
+                self.milchlisten_dict['vorrats_bestellmengen'][add_vorrat_index] = float(mind_best)
+            except:
+                self.milchlisten_dict['vorrats_bestellmengen'][add_vorrat_index] = 1.
+
+
+            # add mindestbestellmenge to each row
+            add_min_best_var = tk.StringVar()
+            self.mind_best_vars += [add_min_best_var]
+            tk.Label(self.main_frame, textvariable = add_min_best_var
+                      ).grid(row=n, column=2, sticky=tk.E, padx=20)
+            # add bestellmenge to each row
+            add_best_var = tk.StringVar()
+            self.best_vars += [add_best_var]
+            tk.Label(self.main_frame, textvariable = add_best_var
+                      ).grid(row=n, column=3, sticky=tk.E, padx=20)
+            # add total bestellmenge to each row
+            tot_best_var = tk.StringVar()
+            try:
+                tot_best_var.set(self.milchlisten_dict['sumorders'].loc[add_vorrat_index].values.flatten()[0])
+            except:
+                tot_best_var.set('0')
+
+            self.tot_best_vars += [tot_best_var]
+            _totlabel = tk.Label(self.main_frame, textvariable = tot_best_var)
+            self.tot_best_menge_labels += [_totlabel]
+            _totlabel.grid(row=n, column=4, sticky=tk.E, padx=20)
+
+            tk.Label(self.main_frame,
+                      text=self.milchlisten_dict['fulllist']['Produkt'][add_vorrat_Qs].values[0]
+                      ).grid(row=n, column=0, sticky=tk.W)
+
+            # add entry-field
+            vorrat_var = tk.StringVar()
+            self.vorrat_vars += [vorrat_var]
+            entry = tk.Entry(self.main_frame, textvariable = vorrat_var)
+            self.vorrat_entrys += [entry]
+            entry.bind("<Return>", add_vorrat)
+            entry.bind("<FocusOut>", add_vorrat)
+            entry.grid(row=n, column=1, padx=20)
+
+            # call read_callback again to read the newly added products
+            read_callback()
+
+
+        # additional vorrat products
+        add_vorrat_set_frame = tk.Frame(self.root)
+        add_vorrat_set_entry = tk.Entry(self.main_frame, #add_vorrat_set_frame,
+                                        textvariable=self.add_vorrat_set_var)
+        add_vorrat_set_entry.insert(0, 'Art. Nr.')
+        add_vorrat_set_entry.grid(row=6, column=0, padx=5, pady=5)
+
+        # remove text on click
+        def rem_text(event):
+            add_vorrat_set_entry.delete(0, tk.END)
+        add_vorrat_set_entry.bind("<Button-1>", rem_text)
+        add_vorrat_set_entry.bind("<Return>", lambda event: add_field())
+
+        tk.Button(self.main_frame, #add_vorrat_set_frame,
+                  text='Vorrat-Produkt hinzufügen',
+                  command=add_field).grid(row=6, column=1, pady=5)
+
+        add_vorrat_set_frame.pack()
+
+        self.add_vorrat_frame.pack()
 
 
         def generate_files():
             # check if folder has been selected
             if 'foldername' not in self.returned_values:
-                warn_variable.set('Zuerst Ordner auswählen!')
-                warnlabel.config(bg='red')
+                self.warn_variable.set('Zuerst Ordner auswählen!')
+                self.warn_label.config(bg='red')
 
             else:
                 # check if all orders are correct
                 bestellmengen_OK = True
                 mind_bestellmengen=[self.milchlisten_dict['vorrats_bestellmengen'][i] for i in self.milchlisten_dict['vorrats_index']]
-                for i, val in enumerate([self.milchlisten_dict['sumorders'].loc[i].values.flatten()[0] for i in self.milchlisten_dict['vorrats_index']]):
-                    if np.round(float(val) + float(vorrat_vars[i].get()), 2) % mind_bestellmengen[i] != 0:
+                for i, best_ind in enumerate(self.milchlisten_dict['vorrats_index']):
+                    try:
+                        val = self.milchlisten_dict['sumorders'].loc[best_ind].values.flatten()[0]
+                    except:
+                        # if index is not in orders
+                        val = '0.'
+
+                    if np.round(float(val) + float(self.vorrat_vars[i].get()), 2) % mind_bestellmengen[i] != 0:
                         bestellmengen_OK = False
-                        tot_best_menge_labels[i].config(bg="red")
+                        self.tot_best_menge_labels[i].config(bg="red")
                     else:
-                        tot_best_menge_labels[i].config(bg="green")
+                        self.tot_best_menge_labels[i].config(bg="green")
 
                 if bestellmengen_OK is False:
-                    warn_variable.set('Bestellmengen passen nicht zu Mindestbestellmengen!')
-                    warnlabel.config(bg='red')
+                    self.warn_variable.set('Bestellmengen passen nicht zu Mindestbestellmengen!')
+                    self.warn_label.config(bg='red')
                 else:
-                    warn_variable.set('Milchlisten werden erzeugt...')
-                    warnlabel.config(bg='green')
+                    self.warn_variable.set('Milchlisten werden erzeugt...')
+                    self.warn_label.config(bg='green')
                     self.root.update_idletasks()
                     _ = getresult(['KW'] + self.vorratsliste + self.additional_vorrat_indexes,
-                                  [KW_var] + vorrat_vars)
+                                  [KW_var] + self.vorrat_vars)
 
                     self.milchlisten_erzeugen()
 
+
         # add a button to generate the lists
-        tk.Button(frame_0, text='Milchlisten erzeugen',
+        tk.Button(self.frame_0, text='Milchlisten erzeugen',
                   command=generate_files).grid(row=0, column=1)
-        frame_0.pack(fill='both', padx=10, pady=10)
+        self.frame_0.pack(fill='both', padx=10, pady=10)
 
 
 
@@ -824,6 +943,6 @@ class gui(milchliste):
 if __name__ is '__main__':
     #asdf = milchliste()
     asdf = gui()
-    #asdf.gui(['Trinkmolke'])
-    asdf.gui(['302714', '110028', '120034'])
+    asdf.gui()
+    #asdf.gui(['302714', '110028', '120034'])
     #print(asdf)
